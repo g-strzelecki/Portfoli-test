7651a97698795725772a1289deb49c57ed72ab79
g-strzelecki <grzegorz.strzelecki84@gmail.com>
2019-01-22 21:56:48 +0100

    After 1st review

--- js/timer.js
+++ js/timer.js
@@ -1,21 +1,27 @@
 function counter() {
     let today = new Date();
-    /* MAIN DATA INFORMATION
+    /*
     var day = today.getDate();
     var month = today.getMonth() + 1;
     var year = today.getFullYear();
     */
 
     let hour = today.getHours();
-    if (hour < 10) hour = "0" + hour;
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
 
     let minute = today.getMinutes();
-    if (minute < 10) minute = "0" + minute;
+    if (minute < 10) (
+        minute = "0" + minute;
+    }
 
     let second = today.getSeconds();
-    if (second < 10) second = "0" + second;
+    if (second < 10) {
+        second = "0" + second;
+    }
 
-    /* WHOLE DATA INFORMATION
+    /*
     document.getElementById("clock").innerHTML = day + "/" + month + "/" + year + " | " + hour + ":" + minute + ":" + second;
     */
 
@@ -26,17 +32,5 @@ function counter() {
 
 document.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed");
+    counter();
 });
-
-for (var i = 0; i < 1000000000; i++) { } // This synchronous script is going to delay parsing of the DOM,
-// so the DOMContentLoaded event is going to launch later.
-
-function doSomething() {
-    console.info("DOM loaded");
-}
-
-if (document.readyState === "loading") {  // Loading hasn't finished yet
-    document.addEventListener("DOMContentLoaded", doSomething);
-} else {  // `DOMContentLoaded` has already fired
-    doSomething();
-}
